897b685fe4525a14f83b67a11ef7f036f95c542e
Luke Hatcher <lukeman@gmail.com>
2009-05-01 13:50:57 -0400

    Script now decodes a file given a valid toot id.

--- tootfiles.py
+++ tootfiles.py
@@ -12,13 +12,17 @@ import re
 import zlib
 import base64
 import md5
+import time
+import urllib
+import simplejson as json
+from BeautifulSoup import BeautifulSoup
 
 # External dependency, see README.md
 import twitter
 
-class FileEncoder(object):
+class TootEncoder(object):
     def __init__(self, filename):
-        super(FileEncoder, self).__init__()
+        super(TootEncoder, self).__init__()
         self.filename = filename
         self._encode()
     
@@ -44,14 +48,34 @@ class FileEncoder(object):
         ''' Given the encoded string, slice it into twitter ready array elements '''
         self.tootcount = int(math.ceil(len(data)/float(n)))
         slices = range(self.tootcount,0,-1)
+        slices.reverse()
+        slices = range(self.tootcount)
+        slices.reverse()
+        print slices
         
         return [data[i*n:(i+1)*n] for i in slices]
     
-    def PublishToot(self, username, password):
+    def publish(self, username, password):
         api = twitter.Api(username, password)
-        
+        print "Publishing '%s' to %s's stream with %s segments..." % (self.filename, username, self.tootcount) 
         for toot in self.tootlist:
-            api.PostUpdate(toot)
+            # Occasionally, Twitter will either throttle us or throw an error.
+            # For now, we'll handle all exceptions and simply retry. This should
+            # be improved in the future.
+            for retries in range(5):
+                try:
+                    #print "Attempting to publish '%s' with length %s" % (toot, len(toot))
+                    status = api.PostUpdate(toot)
+                    print '.',
+                    break
+                except:
+                    if retries == 4:
+                        raise Exception('Unable to post a segment. Quitting.')
+                    else:
+                        # Let's wait a bit before trying again
+                        time.sleep(1)
+        print "Finished."
+                    
     
     def __unicode__(self):
         for toot in self.tootlist:
@@ -59,21 +83,82 @@ class FileEncoder(object):
                 
         
 class TootDecoder(object):
-    def __init__(self, tootdata):
+    def __init__(self, headerid):
         super(TootDecoder, self).__init__()
-        self._processheader()
+        self.headerid = headerid
+        self.tootlist = []
+        self._grabheader()
+        self._getinfo()
     
-    def _get_info(tootarray):
-        header_re = "|Tootfile:'(?P<filename>.*?)' MD5:'(?P<md5>.*?)' Count:'(?P<count>.*?)'|"
-        results = re.compile(header_re).search(tootarray[0])
+    def retrieve(self):
+        """Retrieve toot data from Twitter"""
+        if self.headerid is not None:
+            self._walk()
+            self._decode()
+    
+    def write(self, filename=None, overwrite=False):
+        if(self.md5hash == self.headerinfo['md5']):
+            if filename is None:
+                filename = self.headerinfo['filename']
+            if not overwrite and os.path.exists(filename):
+                raise Exception("File exists. Will not overwrite. Specify permission with overwrite set to true.")
+            f = open(filename,'wb')  
+            f.write(self.rawdata)
+            f.close()
+            
+    
+    def _grabheader(self):
+        """Takes the headerid and retrieves the header text"""
+        raw = urllib.urlopen('http://twitter.com/statuses/show/%s.json' % self.headerid)
+        js = raw.readlines()
+        js_object = json.loads(js[0])
+        self.headerdata = js_object['text']
+        #print self.headerdata
+        self.username = js_object['user']['screen_name']
+    
+    def _getinfo(self):
+        header_re = "\|Tootfile:'(?P<filename>.*?)' MD5:'(?P<md5>.*?)' Count:'(?P<count>\d+)'\|"
+        results = re.match(header_re, self.headerdata)
         self.headerinfo = results.groupdict()
+    
+    def _walk(self):
+        grabbedtoots = 0
+        
+        url = 'http://twitter.com/%s?page=%s'
+        re_status_id = re.compile(r'.*/status/([0-9]*).*')
+        
+        maxpages = int(math.ceil(int(self.headerinfo['count'])/20.0))
+        for page in range(1,maxpages+1):
+            f = urllib.urlopen(url % (self.username, page))
 
+            soup = BeautifulSoup(f.read())
+            f.close()
+            toots = soup.findAll('li', {'class': re.compile(r'.*\bstatus\b.*')})
+            if len(toots) == 0:
+                break
 
-def decode_array(tootarray):
-    data = "".join(tootarray[1:])
-    compressed_data = base64.b64decode(data)
-    raw_data = zlib.decompress(compressed_data)
-    return raw_data
+            for toot in toots:
+                if grabbedtoots < int(self.headerinfo['count']):
+                    m = re_status_id.search(toot.find('a', 'entry-date')['href'])
+                    status_id = m.groups()[0]
+
+                    if(int(status_id) < int(self.headerid)):
+                        data = str(toot.find('span', 'entry-content').renderContents())
+                        self.tootlist.append(data)
+                        grabbedtoots += 1
+                else:
+                    break
+
+            # small delay between pages 
+            time.sleep(1)
+    def _decode(self):
+        """Decodes the raw data from the Twitter stream"""
+        data = "".join(self.tootlist)
+        compressed_data = base64.b64decode(data)
+        self.rawdata = zlib.decompress(compressed_data)
+        self.md5hash = md5.new(self.rawdata).hexdigest()
+        
+        
      
 
 def main():
